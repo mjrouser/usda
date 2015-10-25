8773283f2a820c05e349e5e50926c42ef3ef6500
mjrouser <matthew@mjrouser.com>
2015-10-24 23:27:04 -0400

    cleanup and unsuccessful geoJSON hookup

--- app/scripts/controllers/mapctrl.js
+++ app/scripts/controllers/mapctrl.js
@@ -8,7 +8,7 @@
  * Controller of the usdaApp
  */
 angular.module('usdaApp')
-  .controller('MapCtrl', function ($scope) {
+  .controller('MapCtrl', function ($scope, $http) {
 
 
 
@@ -16,19 +16,38 @@ angular.module('usdaApp')
         defaults: {
             scrollWheelZoom: false,
             maxZoom: 18,
+            minZoom: 4,
             tileLayer: 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ',
             tileLayerOptions: {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.light'
-    },
+               attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		  '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		  'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		  id: 'mapbox.light',
+		  detectRetina: true,
+		  reuseTiles: true
+            },
         },
-        center: {
+        usa: {
            lat: 37.8,
            lng: -96,
            zoom: 4
         }
+    });
+
+    $http.get("scripts/data/us-states.js").success(function(data, status) {
+        angular.extend($scope, {
+            geojson: {
+                data: statesData,
+                style: {
+                    fillColor: "green",
+                    weight: 2,
+                    opacity: 1,
+                    color: 'white',
+                    dashArray: '3',
+                    fillOpacity: 0.7
+                }
+            }
+        });
     });
 
 
